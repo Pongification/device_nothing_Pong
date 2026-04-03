b43db0e9d6f355edae200997a07f34a1f300874b
Ghosuto <clash.raja10@gmail.com>
2026-04-03 09:46:41 +0000

    Pong: vibrator: Tune haptic strength
    Signed-off-by: GHOST | ゴースト <90827302+Ghosuto@users.noreply.github.com>

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -13,9 +13,9 @@
 
 #include "aac_vibra_function.h"
 
-#define RICHTAP_LIGHT_STRENGTH 79
-#define RICHTAP_MEDIUM_STRENGTH 89
-#define RICHTAP_STRONG_STRENGTH 99
+#define RICHTAP_LIGHT_STRENGTH 82
+#define RICHTAP_MEDIUM_STRENGTH 91
+#define RICHTAP_STRONG_STRENGTH 100
 
 enum vibrationMode {
     MODE_NONE,
